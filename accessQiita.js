29c7bfc0406dd6f2cca10d8232041a4b6a8849a8
Yudai Jinno <nakou36@gmail.com>
2022-11-23 18:26:31 +0900

    コミットし直し

---
+++ accessQiita.js
@@ -0,0 +1,29 @@
+//基本パラメータ
+//後で変更できるように外出ししておく。
+const PAGE = 1;
+const PER_PAGE = 4;
+const STOCKS = 10;
+const axios = require("axios");
+
+const accessQiita = async () => {
+  const targetDate = new Date();
+  //今日の日付から一週間前を設定
+  targetDate.setDate(targetDate.getDate() - 7);
+  //YYYY-MM-DD部分だけ抽出
+  const weekBeforeDay = targetDate.toISOString().split("T")[0];
+  const qiitaURL = `https://qiita.com/api/v2/items?page=${PAGE}&per_page=${PER_PAGE}&query=created:>${weekBeforeDay}+stocks:>${STOCKS}`;
+
+  const result = await axios.get(qiitaURL);
+  const journalList = result.data.map(journal => {
+    return {
+        title:journal.title,
+        url:journal.url
+    }
+  })
+  console.log(journalList)
+  return journalList;
+};
+
+accessQiita();
+
+module.exports = accessQiita;
---
+++ index.js
@@ -0,0 +1,34 @@
+const line = require("@line/bot-sdk");
+require("dotenv").config();
+const accessQiita = require("./accessQiita");
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+const client = new line.Client(config);
+
+exports.helloPubSub = async (event, context) => {
+  const journals = await accessQiita();
+  const journalsMessages = journals.map((journal) => {
+    return {
+      type: "text",
+      text: "タイトル：" + journal.title + "URL:" + journal.url,
+    };
+  });
+  const messages = [
+    {
+      type: "text",
+      text: "本日の上位記事5件です！",
+    },
+    ...journalsMessages,
+  ];
+
+  try {
+    const res = await client.broadcast(messages);
+    console.log(res);
+  } catch (error) {
+    console.log(`エラー: ${error.statusMessage}`);
+    console.log(error.originalError.response.data);
+  }
+};
