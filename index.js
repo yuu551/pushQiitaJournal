1acc6b23abd8df701e8111679cbec7b55c635197
Yudai Jinno <nakou36@gmail.com>
2022-11-26 19:52:37 +0900

    テスト用の関数を削除

--- index.js
+++ index.js
@@ -28,25 +28,3 @@ exports.helloPubSub = async (event, context) => {
     console.log(error.originalError.response.data);
   }
 };
-
-const helloPubSub2 = async (event, context) => {
-  const journals = await getJournals();
-  const journalsMessages = makeMessages(journals)
-  const messages = [
-    {
-      type: "text",
-      text: "本日の上位記事3件です！",
-    },
-    ...journalsMessages,
-  ];
-
-  try {
-    const res = await client.broadcast(messages);
-    console.log(res);
-  } catch (error) {
-    console.log(`エラー: ${error.statusMessage}`);
-    console.log(error.originalError.response.data);
-  }
-};
-
-helloPubSub2()
